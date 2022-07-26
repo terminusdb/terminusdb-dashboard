7a25dc24ec1f770a11a8d264f651931920a81a1b
Robin de Rooij <rderooij685@gmail.com>
2022-07-26 11:22:41 +0200

    Add CI for dashboard

---
+++ .ci/clean_cloudflare_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+import sys
+
+files = [
+    'https://dashboard-dev.terminusdb.com/',
+    'https://dashboard-dev.terminusdb.com/verify',
+    'https://dashboard-dev.terminusdb.com/index.html',
+    'https://dashboard-dev.terminusdb.com/tdb-dashboard.min.js',
+    'https://dashboard-dev.terminusdb.com/App.css',
+    'https://dashboard.terminusdb.com/tdb-dashboard.min.js',
+    'https://dashboard.terminusdb.com/App.css',
+    'https://dashboard.terminusdb.com/',
+    'https://dashboard.terminusdb.com/verify',
+    'https://dashboard.terminusdb.com/index.html',
+]
+
+# We don't need to wipe the production cache on every commit
+if not sys.argv[1].startswith("refs/tags"):
+    files = [x for x in files if "dashboard-dev" in x]
+
+headers = {
+    'X-Auth-Email': os.environ["CLOUDFLARE_EMAIL"],
+    'X-Auth-Key': os.environ["CLOUDFLARE_API_KEY"],
+}
+zone = os.environ["CLOUDFLARE_ZONE"]
+print(requests.post(f"https://api.cloudflare.com/client/v4/zones/{zone}/purge_cache", json={'files': files}, headers=headers).text)
---
+++ .ci/install_dependencies.sh
@@ -0,0 +1,8 @@
+cd packages/tdb-dashboard
+npm i
+cd ../tdb-react-layout
+npm i
+cd ../tdb-react-components
+npm i
+cd ../react-worker
+npm i
