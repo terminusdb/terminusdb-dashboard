fe2562d149ebc8ad8f64ee3914179738be311166
Francesca-Bit <francesca@terminusdb.com>
2022-08-16 13:50:58 +0200

    fix error reporting for local_dashboard

--- packages/tdb-dashboard/src/pages/Home.js
+++ packages/tdb-dashboard/src/pages/Home.js
@@ -1,14 +1,17 @@
-import React from "react"
+import React,{useState} from "react"
 import { Layout } from "./Layout"
 import {NoDataProductSelectedStyle} from "../components/constants"
 import {WOQLClientObj} from '../init-woql-client'
 import {Card, Row, Col, Button,Container} from "react-bootstrap"
 import {useNavigate} from "react-router-dom"
 import {RiTeamLine} from "react-icons/ri"
+import {NewTeamModal} from "../components/NewTeamModal"
+import {BsFillPeopleFill} from "react-icons/bs"
 
 export const Home = () => {
-    const {woqlClient,changeOrganization} = WOQLClientObj()
+    const {woqlClient,changeOrganization,clientUser} = WOQLClientObj()
     if(!woqlClient) return ""
+    const [showNewMemberModal, setShowNewMemberModal] = useState(false)
 
     const teamList = woqlClient.userOrganizations()
     let navigate = useNavigate()
@@ -24,11 +27,19 @@ export const Home = () => {
                     <Col xs={12} className="text-center d-block align-items-center justify-content-center">
                         <h3 className="text-success mt-5 mb-4">
                             <RiTeamLine className="mr-2"/> Select a Team
-                        </h3>
+                            { clientUser && clientUser.connection_type !== "LOCAL" &&
+                            <React.Fragment>
+                                <Button id="team_page_open_create_new_team_modal" className="ml-4 btn-info mt-4 mb-4" onClick={()=>{setShowNewMemberModal(true)}}>
+                                    <BsFillPeopleFill className="mr-2"/>Create a new Team
+                                </Button>
+                                <NewTeamModal show={showNewMemberModal} setShow={setShowNewMemberModal} />
+                            </React.Fragment>
+                            }
+                        </h3>                      
                         <Row>
                         {teamList.map((item,index) =>{
                             return <Col md={4} className="py-2 doc-summary-card" key={`key___${index}`}>
-                            <Button className="bg-transparent border-0 p-0 w-100" onClick={(e) => changeOrganizationHandler(item.name)}>
+                            <Button id={item.name} className="bg-transparent border-0 p-0 w-100" onClick={(e) => changeOrganizationHandler(item.name)}>
                                 <Card bg="dark" style={{maxHeight: "220px", cursor: "pointer"}} >
                                     <Card.Header className="bg-transparent border-0 d-flex text-wrap">
                                     </Card.Header>
