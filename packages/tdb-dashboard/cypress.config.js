0f15b2f0a346b3db6451ccd23129374caf3f4f08
Francesca-Bit <francesca@terminusdb.com>
2022-10-20 11:58:54 +0100

    update client version

--- packages/tdb-dashboard/cypress.config.js
+++ packages/tdb-dashboard/cypress.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  env: {
-  "MAILSLURP_API_KEY": "134d181528f7c1d7d730894948b5ea9faa117c4b8a580e1cbd805a93ce27a9e7",
-  "COLLABORATOR_USER":"collaborator@gmail.com",
-  "COLLABORATOR_PASS":"collaborator@gmail.com",
-  "OWNER_PASS": "GEHEHUWBZBSBVD&#*JSHHDHBSHBNJ",
-  "NEW_PASS" : "6789UWBZBSBVD&#*JSHHDHBSHBNJ"},
+  env:{},
   defaultCommandTimeout: 120000,
   responseTimeout: 120000,
   requestTimeout: 120000,
@@ -15,7 +10,7 @@ module.exports = {
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'https://dashboard.terminusdb.com/',
+    baseUrl: 'https://dashboard-dev.terminusdb.com/',
    
 
   },
