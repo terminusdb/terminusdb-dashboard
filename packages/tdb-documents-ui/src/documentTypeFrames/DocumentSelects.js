36b11f3c94d7b18db9278e52e7793770c9e0fb9f
Francesca-Bit <francesca@terminusdb.com>
2022-11-22 15:15:13 +0000

    review table

--- packages/tdb-documents-ui/src/documentTypeFrames/DocumentSelects.js
+++ packages/tdb-documents-ui/src/documentTypeFrames/DocumentSelects.js
@@ -132,11 +132,11 @@ export const DocumentSearch = ({label, onChange, value, required, linked_to, dis
                 </Button>
             </div>
         </Stack>
-        {selected && selected.original && <>
+        {selected  && <>
             <AiOutlineCheck className="text-success mr-2"/>
             <small>{"Selected: "}</small>
             <div className="d-flex tdb__input">
-                <label className="text-decoration-underline">{selected.original.id}</label>
+                <label className="text-decoration-underline">{selected.id}</label>
                 {!required && <Button className="btn btn-sm bg-transparent border-0 text-danger"
                         title={`Click here to search for a ${linked_to}`}
                         onClick={handleClear}>
