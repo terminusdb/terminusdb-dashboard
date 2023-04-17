9e4414e65d0221ad7854240ee907c2757025704f
Kitty Jose <kitty@terminusdb.com>
2023-04-17 12:38:36 +0100

    cleanup

---
+++ packages/tdb-documents-ui-template/src/components/infoCloseButton.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { FaTimes } from "react-icons/fa"
+import { Button } from "react-bootstrap"
+
+/**
+ * 
+ * @param {*} type setShowFrames constant to hide display of document frames
+ * @returns a close button icon
+ */
+ export const CloseInfoButton = ({ setShowInfo }) => { 
+
+  function handleClose (e) {
+      let info = { frames: false, history: false }
+      setShowInfo(info)
+  }
+  // on close button display document list table
+  return <Button variant="light" 
+    className="btn-sm btn text-dark float-right" 
+    tilte={`Close`}
+    onClick={handleClose}>
+    <FaTimes/>
+  </Button>
+}
