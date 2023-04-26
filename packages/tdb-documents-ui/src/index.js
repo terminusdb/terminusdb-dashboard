f90e55c8188009cc632e69408fab1744e1e6e9f9
Kitty Jose <kitty@terminusdb.com>
2023-04-26 12:31:55 +0100

    my stuff

--- packages/tdb-documents-ui/src/index.js
+++ packages/tdb-documents-ui/src/index.js
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/terminusdb__styles.css';
+
 export { FrameViewer } from './FrameViewer'
 export { DiffViewer } from './diffs/diffViewer'
 export { DocumentsUIHook } from './hook/DocumentsUIHook'
+ 
