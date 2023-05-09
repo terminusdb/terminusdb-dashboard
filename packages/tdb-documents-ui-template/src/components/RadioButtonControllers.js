d64ae80602f2458e03967cb78550eee634ba5c65
Kitty Jose <kitty@terminusdb.com>
2023-05-09 10:47:20 +0100

    adding controller diff view file

---
+++ packages/tdb-documents-ui-template/src/components/RadioButtonControllers.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Stack from "react-bootstrap/Stack"
+import Badge from 'react-bootstrap/Badge';
+
+export const RadioButtonControllers = ({ commitID, handleChange, compareChecked, displayText, recentCommit }) => {
+	return <Stack gap={3} direction="horizontal" className="w-100">
+    <input
+      type="radio"
+      name={commitID}
+      checked={compareChecked.curr.identifier === commitID || compareChecked.prev.identifier === commitID}
+      onChange={(e) => handleChange(commitID)}
+    />
+    <label className="control-label w-100 mt-2">{displayText}	</label>
+    {recentCommit && <Badge bg="success" className="text-dark">Latest</Badge>}
+	</Stack>
+}
