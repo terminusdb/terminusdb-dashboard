a9c98048997eeb4d4b694306674570fade98a2a4
Kitty Jose <kitty@terminusdb.com>
2023-06-23 11:11:28 +0100

    changes

--- packages/tdb-dashboard/src/components/DisplayNoIndexingAction.js
+++ packages/tdb-dashboard/src/components/DisplayNoIndexingAction.js
@@ -7,8 +7,8 @@ import { BsToggleOn, BsSearch, BsKey } from "react-icons/bs"
 import { RxCheckbox } from "react-icons/rx"
 
 const StepDetail = ({ stepNumber, stepComponent, icon}) => {
-  return <Card className="bg-transparent p-3 mb-3 border border-secondary justify-content-center"
-    style={{height: "10.5rem"}}>
+  return <Card className="bg-transparent mb-3 border border-secondary justify-content-center"
+    style={{height: "9rem"}}>
     <Card.Body>
       <Stack>
         <h3 className="text-light fw-bold">{icon}</h3>
@@ -53,10 +53,20 @@ export const DisplayNoIndexingAction = ({ helpDescription }) => {
     <h4 className="text-light fw-bold">Get started with Indexing Actions</h4>
     <h5 className="text-light fw-bold">{helpDescription}</h5>
     <Container className="my-5">
-      <h6 className="text-light">
+      <h6 className="text-light small">
         {`After following the below steps, you can start Indexing your data using the Change Request workflow`}
       </h6>
       <Row>
+        <Col md={3}></Col>
+        <Col md={6}>
+          <StepDetail stepNumber={1} stepComponent={getStep1()} icon={<BsKey/>}/>
+          <StepDetail stepNumber={2} stepComponent={getStep2()} icon={<RiUserFollowFill/>}/>
+          <StepDetail stepNumber={3} stepComponent={getStep3()} icon={<RxCheckbox/>}/>
+          <StepDetail stepNumber={4} stepComponent={getStep4()} icon={<BsSearch/>}/>
+        </Col>
+        <Col md={3}></Col>
+      </Row>
+      {/*<Row>
         <Col md={6}><StepDetail stepNumber={1} stepComponent={getStep1()} icon={<BsKey/>}/></Col>
         <Col md={6}><StepDetail stepNumber={2} stepComponent={getStep2()} icon={<RiUserFollowFill/>}/></Col>
       </Row>
@@ -72,7 +82,7 @@ export const DisplayNoIndexingAction = ({ helpDescription }) => {
 
       
 
-      </Row>
+      </Row>*/}
       
     </Container>
   </div>
