95c5ce6aa6011ad1970f2d8e98aa73d86993d036
Francesca-Bit <francesca@terminusdb.com>
2023-06-28 15:33:05 +0100

    add message

--- packages/tdb-dashboard/src/components/DisplayNoIndexingAction.js
+++ packages/tdb-dashboard/src/components/DisplayNoIndexingAction.js
@@ -4,10 +4,13 @@ import Stack from "react-bootstrap/Stack"
 import { useParams, NavLink} from "react-router-dom";
 import { RiUserFollowFill } from "react-icons/ri"
 import { BsToggleOn, BsSearch, BsKey } from "react-icons/bs"
+import {SiHandlebarsdotjs} from "react-icons/si"
 import { useOpenAI } from "../hooks/useOpenAI";
 import { RxCheckbox } from "react-icons/rx"
+import {WOQLClientObj} from '../init-woql-client'
 
 const StepDetail = ({ stepNumber, stepComponent, icon}) => {
+  
   return <Card className="bg-transparent mb-3 border border-secondary justify-content-center"
     style={{height: "9rem"}}>
     <Card.Body>
@@ -22,38 +25,41 @@ const StepDetail = ({ stepNumber, stepComponent, icon}) => {
 }
 
 
-function getStep1() {
-  return <>
-    Visit <NavLink href="https://platform.openai.com/account/api-keys" className={"mr-1"}>Open AI</NavLink>
-    and create a new Secret API Key
-  </>
+function getStep(stepNumber,supMode) {
+  let { organization,dataProduct } = useParams()
+  const stepOptions= {
+    "step1":<>
+              Visit <NavLink href="https://platform.openai.com/account/api-keys" className={"mr-1"}>Open AI</NavLink>
+              and create a your Secret API Key
+            </>,
+    "step2": {"LOCAL":<>
+                Copy and paste your generated OpenAI API Key in your environment variables, restart the application
+              </>,
+              "REMOTE":<>
+                Copy and paste your generated OpenAI API Key in your <NavLink to={`/${organization}/profile`} className={"mr-1"}>Profile page</NavLink>
+                </>
+              },
+    "step3":<>
+            Go to <NavLink to={`/${organization}/${dataProduct}/openai_configuration`} className={"mr-1"}>Open AI configuration </NavLink> page
+            to add a graphql query and an handlebar template for every Document
+            </>,
+    "step4": <>
+              In your profile turn on the Indexing button next to the OpenAI Key field.
+              All data products in your team will begin to index.
+              </>,
+    "step5": <>
+              Use VectorLink to Semantically search your data products and use the vector embeddings to talk to OpenAI.
+              Click <NavLink href="https://terminusdb.com/docs/" className={"mr-1"}>here</NavLink>for more info on how to use VectorLink.
+              </>
+  }
+  const step = stepOptions[`step${stepNumber}`]
+  return supMode ? step[supMode] : step
 }
 
-function getStep2() {
-  let { organization } = useParams()
-  return <>
-   Copy and paste your generated OpenAI API Key in your <NavLink to={`/${organization}/profile`} className={"mr-1"}>Profile page</NavLink>
-  </>
-}
-
-function getStep3() {
-  return <>
-    In your profile turn on the Indexing button next to the OpenAI Key field. All data products in your team will begin to index.
-  </>
-}
-
-function getStep4() {
-  return <>
-    Use VectorLink to Semantically search your data products and use the vector embeddings to talk to OpenAI.
-    Click <NavLink href="https://terminusdb.com/docs/" className={"mr-1"}>here</NavLink>for more info on how to use VectorLink.
-    </>
-}
 
 export const DisplayNoIndexingAction = ({ helpDescription }) => {
-  const {  hasKey } = useOpenAI()
-  const { organization } = useParams()
-
-  //console.log("hasKey",)
+  const {clientUser} = WOQLClientObj()
+  const connection_type = clientUser.connection_type==="LOCAL" ? "LOCAL" : "REMOTE"
   
   return <div className="text-center justify-content-center">
     <h4 className="text-light fw-bold">Get started with Indexing Actions</h4>
@@ -65,31 +71,14 @@ export const DisplayNoIndexingAction = ({ helpDescription }) => {
       <Row>
         <Col md={3}></Col>
         <Col md={6}>
-          <StepDetail stepNumber={1} stepComponent={getStep1()} icon={<BsKey/>}/>
-          <StepDetail stepNumber={2} stepComponent={getStep2()} icon={<RiUserFollowFill/>}/>
-          <StepDetail stepNumber={3} stepComponent={getStep3()} icon={<RxCheckbox/>}/>
-          <StepDetail stepNumber={4} stepComponent={getStep4()} icon={<BsSearch/>}/>
+          <StepDetail stepNumber={1} stepComponent={getStep(1)} icon={<BsKey/>}/>
+          <StepDetail stepNumber={2} stepComponent={getStep(2, connection_type)} icon={<RiUserFollowFill/>}/>
+          <StepDetail stepNumber={3} stepComponent={getStep(3)} icon={<SiHandlebarsdotjs/>}/>
+          <StepDetail stepNumber={4} stepComponent={getStep(4)} icon={<RxCheckbox/>}/>
+          <StepDetail stepNumber={5} stepComponent={getStep(5)} icon={<BsSearch/>}/>
         </Col>
         <Col md={3}></Col>
       </Row>
-      {/*<Row>
-        <Col md={6}><StepDetail stepNumber={1} stepComponent={getStep1()} icon={<BsKey/>}/></Col>
-        <Col md={6}><StepDetail stepNumber={2} stepComponent={getStep2()} icon={<RiUserFollowFill/>}/></Col>
-      </Row>
-      <Row>
-        <Col md={6}>
-          
-          <StepDetail stepNumber={3} stepComponent={getStep3()} icon={<RxCheckbox/>}/>
-          
-        </Col>
-        <Col md={6}>
-        <StepDetail stepNumber={4} stepComponent={getStep4()} icon={<BsSearch/>}/>
-        </Col>
-
-      
-
-      </Row>*/}
-      
     </Container>
   </div>
 }
